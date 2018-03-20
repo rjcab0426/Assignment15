21926ee7be98bdee832cc46ae00250a9d26524b0
Reman <rjcab0426@yahoo.com>
2018-03-20 16:47:20 -0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { trigger, transition, group, query, style, animate } from '@angular/animations';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('routeAnimation', [
+        transition('1 => 2, 2 => 3', [
+            style({ height: '!' }),
+            query(':enter', style({ transform: 'translateX(100%)' })),
+            query(':enter, :leave', style({ position: 'absolute', top: 0, left: 0, right: 0 })),
+           
+            group([
+                query(':leave', [
+                    animate('0.3s cubic-bezier(.35,0,.25,1)', style({ transform: 'translateX(-100%)' })),
+                ]),
+                
+                query(':enter', animate('0.3s cubic-bezier(.35,0,.25,1)', style({ transform: 'translateX(0)' }))),
+            ]),
+        ]),
+        transition('3 => 2, 2 => 1', [
+            style({ height: '!' }),
+            query(':enter', style({ transform: 'translateX(-100%)' })),
+            query(':enter, :leave', style({ position: 'absolute', top: 0, left: 0, right: 0 })),
+            
+            group([
+                query(':leave', [
+                    animate('0.3s cubic-bezier(.35,0,.25,1)', style({ transform: 'translateX(100%)' })),
+                ]),
+                
+                query(':enter', animate('0.3s cubic-bezier(.35,0,.25,1)', style({ transform: 'translateX(0)' }))),
+            ]),
+        ]),
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app!';
+
+  getDepth(outlet) {
+    return outlet.activatedRouteData['depth'];
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, CanActivate } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 import { FormComponent } from './form/form.component';
+import { RegisteredComponent } from './registered/registered.component';
+import { StudentDetailComponent } from './student-detail/student-detail.component';
 
 import { FormService } from './form.service';
-import { RegisteredComponent } from './registered/registered.component';
 import { CoursetypeService } from './coursetype.service';
 import { CapitalizePipe } from './capitalize.pipe';
 import { SearchPipe } from './search.pipe';
-import { StudentDetailComponent } from './student-detail/student-detail.component';
+import { AuthGuardService } from './auth-guard.service';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/form', pathMatch: 'full'},
-  { path: 'form', component: FormComponent },
-  { path: 'registered', component: RegisteredComponent },
-  { path: 'studentDetail/:id', component: StudentDetailComponent}
+  { path: '', redirectTo: 'root', pathMatch: 'full' }, 
+  { path: 'login', component: LoginComponent, data: {depth: 1} },
+  { path: 'form', component: FormComponent, canActivate: [AuthGuardService], data: {depth: 2} },
+  { path: 'registered', component: RegisteredComponent, canActivate: [AuthGuardService], data: {depth: 3} },
+  { path: 'studentDetail/:id', component: StudentDetailComponent }
 ];
 
 @NgModule({
@@ -27,15 +31,17 @@ const routes: Routes = [
     RegisteredComponent,
     CapitalizePipe,
     SearchPipe,
-    StudentDetailComponent
+    StudentDetailComponent,
+    LoginComponent
   ],
 
   imports: [
     BrowserModule,
     FormsModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [FormService, CoursetypeService],
+  providers: [FormService, CoursetypeService, AuthGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
